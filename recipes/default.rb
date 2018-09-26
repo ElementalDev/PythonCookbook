e7348523519f3edfe1010261a3c4211bb8c9ecec
Reece_Collett <rcollett@spartaglobal.com>
2018-09-26 16:44:17 +0100

    Installing Python, Pip and its dependencies

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,19 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+package "python2.7" do
+  action :install
+end
+
+apt_update "update" do
+  action :update
+end
+
+package "python-pip" do
+  action :install
+end
+
+package "libncurses5-dev" do
+  action :install
+end
