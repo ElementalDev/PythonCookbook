c74797be8c82956213e6a7c1c9890955ce3ba4d5
Reece_Collett <rcollett@spartaglobal.com>
2018-09-26 16:49:19 +0100

    Tests run and completed

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -4,3 +4,16 @@
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
+
+describe package("python") do
+  it {should be_installed}
+  its("version") {should match /2.[0-9.]*/}
+end
+
+describe package("python-pip") do
+  it {should be_installed}
+end
+
+describe package("libncurses5-dev") do
+  it {should be_installed}
+end
